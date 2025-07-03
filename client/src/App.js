24c4e48c13fe8d6e6c016522b6c330001c7ce383
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2025-07-03 18:01:35 +0100

    Fix: store access token and clean up URL after TikTok auth

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,17 @@ const App = () => {
     useEffect(() => {
         const params = new URLSearchParams(window.location.search);
         const token = params.get("access_token");
-        if (token) {
+
+        console.log("URL token param:", token);
+        const storedToken = localStorage.getItem("tiktok_access_token");
+        console.log("Stored token:", storedToken);
+
+        if (token && !storedToken) {
             localStorage.setItem("tiktok_access_token", token);
+            console.log("Access token stored in localStorage");
             // Clean the URL after saving the token
             window.history.replaceState({}, "", "/");
+            console.log("URL cleaned of token");
         }
     }, []);
 
